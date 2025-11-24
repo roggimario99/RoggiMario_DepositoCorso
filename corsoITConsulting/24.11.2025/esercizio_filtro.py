dafb8703a09f5658fbb17e7654ed2cc87feaa940
roggimario99-commits <roggimario99@gmail.com>
2025-11-24 13:36:23 +0100

    Update esercizio_filtro.py

--- corsoITConsulting/24.11.2025/esercizio_filtro.py
+++ corsoITConsulting/24.11.2025/esercizio_filtro.py
@@ -1,26 +1,35 @@
-def is_larger_vector(myList):
-    res = []
-    for i in range(len(myList)-2):
-        res.append( bool(myList[i] + myList[i+1] > myList[i+2]) )  
-    
-    res.append(bool( 2*myList[-2] > myList[-1])  ) #penultimo elemento utilizzo se stesso due volte
-    res.append( True ) #l'ultimo lo metto sempre vero
-    
-    return res
+import random
 
-        
-def is_larger(i):
+N = 40
+numbers = [*range(N)]
+#random.shuffle(numbers)
 
-    return bool(myList[i] + myList[i+1] > myList[i+2]) 
+# funzione di filtro: prende un indice x
+def sum_greater_then_next(x):
     
-    res.append(bool( 2*myList[-2] > myList[-1])  ) #penultimo elemento utilizzo se stesso due volte
-    res.append( True ) #l'ultimo lo metto sempre vero
+    """Implementa la condizione el_x + el_x+1 > el_x+2 con condizione cicliche all'estremo dell'array!"""
+    if x == (len(numbers) - 2): 
+        return numbers[-1] + numbers[0] > numbers[1] #penultimo elemento 
     
-    return res        
+    elif x == (len(numbers) - 1): 
+        return numbers[-2] + numbers[-1] > numbers[1] #ultimo elemento
+    
+    else:
+        return numbers[x] + numbers[x + 1] > numbers[x + 2] #condizione standard
         
-        
-numbers = [1,4,2,3,7,1,3,5,6]
-filtered_numbers = list(filter(is_larger, numbers))
 
-print( filtered_numbers )
-    
+
+
+indici = range( len(numbers) )
+
+indici_validi = list( filter(sum_greater_then_next, indici) )
+
+array_filtrato = [
+    numbers[i] for i in indici_validi
+]
+
+
+print("Array di partenza:", numbers) 
+print("Indici validi:", indici_validi) 
+print("Array filtrato:", array_filtrato) 
+
