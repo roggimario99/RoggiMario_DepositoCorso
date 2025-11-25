4503599e37ce207da5c784cb7e5113eb5f114e1b
roggimario99-commits <roggimario99@gmail.com>
2025-11-25 12:31:28 +0100

    organizzazione

---
+++ corsoITConsulting/25.11.2025/es2/broadcasting.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr = np.arange(0,10,1)
+arr2 = np.arange(0,2,1)
+
+scalar = 4
+print(arr + scalar)
+print(arr * scalar)
+print(scalar * arr)
+
+print(arr + arr2)
---
+++ corsoITConsulting/25.11.2025/esercizio/es1.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+arr = np.random.randint(1, 100 + 1, 15)
+
+sum = np.sum(arr)
+mean = np.mean(arr)
+
+print(f"sum: {sum} mean: {mean}")
